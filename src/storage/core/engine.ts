a1f18ec1c76d1233a695dba536a27e1e5454f416
Rhys Le <tuantai0625@gmail.com>
2026-07-13 11:15:32 +0700

    chore: add todo for dynamic storage id

--- src/storage/core/engine.ts
+++ src/storage/core/engine.ts
@@ -1,5 +1,6 @@
 import { createMMKV } from 'react-native-mmkv'
 
+// TODO: implement dynamic storage id here instead of hardcoded
 const STORAGE_ID = 'currency-converter-storage'
 
 export const storage = createMMKV({
