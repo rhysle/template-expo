fc4d749dd113bd03cfd23cec25e72a80bcb6f527
Rhys Le <tuantai0625@gmail.com>
2026-07-13 11:53:22 +0700

    feat: define storage id dynamically

--- src/storage/core/engine.ts
+++ src/storage/core/engine.ts
@@ -1,7 +1,7 @@
+import Constants from 'expo-constants'
 import { createMMKV } from 'react-native-mmkv'
 
-// TODO: implement dynamic storage id here instead of hardcoded
-const STORAGE_ID = 'currency-converter-storage'
+const STORAGE_ID = `${Constants.expoConfig?.slug ?? 'app'}-storage`
 
 export const storage = createMMKV({
   id: STORAGE_ID,
