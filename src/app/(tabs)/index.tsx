a18f3fe030ae798e0874ad33e09b6a8f9c428634
Rhys Le <tuantai0625@gmail.com>
2026-07-14 20:57:08 +0700

    feat: update HomeScreen dummy layout

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,13 +1,23 @@
 import { View } from 'react-native'
 
 import { Text } from '@/components/base'
-import { useCommonStyles } from '@/theme'
+import { createThemedStyles, useThemedStyles } from '@/theme'
 
 export default function HomeScreen() {
-  const commonStyles = useCommonStyles()
+  const styles = useThemedStyles(createStyles)
   return (
-    <View style={commonStyles.screen}>
+    <View style={styles.container}>
       <Text>Sample Text</Text>
     </View>
   )
 }
+
+const createStyles = createThemedStyles((t) => ({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: t.spacing.lg,
+    backgroundColor: t.colors.background.base,
+  },
+}))
