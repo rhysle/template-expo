e7619f84e016c816615faf03ede41c9879998bd6
Rhys Le <tuantai0625@gmail.com>
2026-09-25 20:23:09 +0700

    feat: update font to Lexend

--- apps/dualzen/src/configs/fonts.ts
+++ apps/dualzen/src/configs/fonts.ts
@@ -7,10 +7,7 @@
 // To check if a font is available in @expo-google-fonts/dev, search the package's source:
 // import { JosefinSlab_100Thin, OpenSans_300Light } from '@expo-google-fonts/dev'
 
-export const FONT_NAME = 'JosefinSlab'
-// export const FONT_NAME = 'Manrope'
-// export const FONT_NAME = 'Outfit'
-// export const FONT_NAME = 'Sora'
+export const FONT_NAME = 'Lexend'
 
 export const runtimeFontFamilyMap = {
   light: `${FONT_NAME}_300Light`,
